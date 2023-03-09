a7a2be025b837f95c799453420033b52577b9b86
Ayushi Rai <ayushicodes@gmail.com>
2023-03-10 02:01:38 +0530

    Ui completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
 import "./App.css";
 
+const projectID = "5c4a0041-a926-48f3-9725-5e095ccd0cf1";
 function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
   return (
     <ChatEngine
       height="100vh"
-      projectID="5c4a0041-a926-48f3-9725-5e095ccd0cf1"
-      userName="ayushicodes"
-      userSecret="704367"
-      renderChatFeed={(ChatAppProps) => <ChatFeed {...ChatAppProps} />}
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 }
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
-export default function TheirMessage() {
+export default function TheirMessage({message,lastMessage}) {
+    const isFirstMessageByUser  = !lastMessage || lastMessage.sender.username !== message.sender.username
   return (
-    <div></div>
+    <div className='message-row' >
+        {isFirstMessageByUser && (<div className='message-avatar' style={{backgroundImage:`url(${message?.sender?.avatar})`}} />
+        )}
+         {message?.attachments.length>0
+        ?(<img
+        src={message.attachments[0].file}
+        alt="message-attachment"
+        className='message-image'
+        style={{marginLeft: isFirstMessageByUser ? '4px' : '48px'}}
+        
+        />):(     <div className='message' style={{float:'left', backgroundColor:'#CABCDC' , marginLeft: isFirstMessageByUser ? '4px' : '48px'}}>{message.text}</div>)}
+    </div>
   )
 }
